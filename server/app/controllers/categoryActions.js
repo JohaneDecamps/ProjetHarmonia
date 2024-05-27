4bc58fd0cf2c465854838cdd59cd63eceb1ae464
JohaneDecamps <johane.decamps@gmail.com>
2024-05-27 15:07:13 +0200

    quete 04

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,60 +1,40 @@
 // Some data to make the trick
 
 const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
-  
+  {
+    id: 1,
+    name: "Science-Fiction",
+  },
+  {
+    id: 2,
+    name: "Comédie",
+  },
+];
 
-  const browse = (req, res) => {
+const browse = (req, res) => {
+  if (req.query.q != null) {
+    const filteredCategories = categories.filter((c) =>
+      c.name.includes(req.query.q)
+    );
 
-    if (req.query.q != null) {
-  
-      const filteredCategories = categories.filter((c) =>
-  
-        c.includes(req.query.q)
-  
-      );
-  
+    res.json(filteredCategories);
+  } else {
+    res.json(categories);
+  }
+};
 
-      res.json(filteredCategories);
-  
-    } else {
-  
-      res.json(categories);
-  
-    }
-  
-  };
+const read = (req, res) => {
+  const parsedId = parseInt(req.params.id, 10);
 
-  const read = (req, res) => {
+  const categorie = categories.find((p) => p.id === parsedId);
 
-    const parsedId = parseInt(req.params.id, 10);
-  
-  
-    const categorie = categories.find((p) => p.id === parsedId);
-  
-  
-    if (categorie != null) {
-  
-      res.json(categorie);
-  
-    } else {
-  
-      res.sendStatus(404);
-  
-    }
-  
-  };
-  
-  
-  // Export it to import it somewhere else
-  
-  module.exports = { browse, read };
-  
+  if (categorie != null) {
+    res.json(categorie);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+// Export it to import it somewhere else
+
+module.exports = { browse, read };
--- server/app/routers/api/categories/router.js
+++ server/app/routers/api/categories/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { browse, read  } = require("../../../controllers/categoryActions");
+const { browse, read } = require("../../../controllers/categoryActions");
 
 router.get("/", browse);
 
